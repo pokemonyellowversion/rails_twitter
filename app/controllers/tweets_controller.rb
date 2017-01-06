634d9e6e182730f6ac1b502113949209bcd7afbe
Holly <hnguyen0711@gmail.com>
2017-01-06 11:21:35 -0800

    created routes and controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,51 @@
+class TweetsController < ApplicationController
+	
+	before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@tweets = Tweet.all
+	end	
+
+	def show
+	end	
+
+	def new
+		@tweet = Tweet.new
+	end	
+
+	def edit
+	end	
+
+	def create
+		@tweet = Tweet.new(tweet_params)	
+
+		if @tweet.save
+			redirect_to tweets_path
+		else
+			render :new
+		end
+	end	
+
+	def update
+		if @tweet.update_attributes(tweet_params)
+			redirect_to tweets_path
+		else
+			render :edit
+		end
+	end	
+
+	def destroy
+		@tweet.destroy
+		redirect_to tweets_path
+	end	
+
+	private
+	def set_tweet
+		@tweet = Tweet.find(params[:id])
+	end	
+	
+	def tweet_params
+		params.require(:tweet).permit(:handle, :content)
+	end
+
+end
